820679cf7fe0043e1eee6eb2e0891250846b2971
Nicolae Moraru <morarunicolae96@gmail.com>
2025-03-19 15:31:47 +0100

    fixed some bugs DONE

--- src/App.jsx
+++ src/App.jsx
@@ -28,7 +28,15 @@ function App() {
 
     }
 
-    function handleDeleteTask() {
+    function handleDeleteTask(id) {
+        setProjectState((prevState) => {
+            return {
+                ...prevState,
+                tasks: prevState.tasks.filter(
+                    (tasks) => tasks.id !== id
+                ),
+            }
+        })
 
     }
 
@@ -113,6 +121,7 @@ function App() {
                 onStartAddProject={handelStartAddProject}
                 projects={projectsState.projects}
                 onSelectProject={handelSelectProject}
+                selectedProjectId={projectsState.selectedProjectId}
             />
             {content}
         </main>
--- src/components/NewTasks.jsx
+++ src/components/NewTasks.jsx
@@ -6,23 +6,24 @@ export default function NewTasks({onAdd}) {
 
     function handleChange(event) {
         setEnteredTask(event.target.value)
-
     }
 
-    function handleClick(){
+    function handleClick() {
         onAdd(enteredTask)
         setEnteredTask('')
 
     }
 
-    return <div className="flex items-center gap-4">
-        <input type="text" className='w-64 px-2 py-1 rounded-sm bg-stone-200'
-               onChange={handleChange}
-               value={enteredTask}
-        />
-        <button className="text-stone-700 hover:text-stone-950"
-                onChange={handleClick}
-        >Add Task
-        </button>
-    </div>
+    return (
+        <div className="flex items-center gap-4">
+            <input type="text" className='w-64 px-2 py-1 rounded-sm bg-stone-200'
+                   onChange={handleChange}
+                   value={enteredTask}
+            />
+            <button className="text-stone-700 hover:text-stone-950"
+                    onClick={handleClick}
+            >Add Task
+            </button>
+        </div>
+    )
 }
